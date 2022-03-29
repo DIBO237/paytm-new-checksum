878e0cc00348380e9a7514af06e6ff484e8b0c58
DIBYO-DEY <63423632+DIBO237@users.noreply.github.com>
2022-03-29 13:11:45 +0530

    added test server

--- index.js
+++ index.js
@@ -91,6 +91,81 @@ app.post('/createOrder', async (req, res) => {
   }
 });
 
+
+app.post('/test/createOrder', async (req, res) => {
+  try {
+    let data = {
+      orderId: req.body.orderId,
+      customerId: req.body.customerId,
+      amount: req.body.amount,
+    };
+    var paytmParams = {};
+    paytmParams.body = {
+      requestType: 'Payment',
+      mid: process.env.TESTMID,
+      orderId: data.orderId,
+      callbackUrl: `https://securegw-stage.paytm.in/theia/paytmCallback?ORDER_ID=${data.orderId}`,
+      txnAmount: {
+        value: data.amount,
+        currency: 'INR',
+      },
+      userInfo: {
+        custId: data.customerId,
+      },
+    };
+
+    /*
+     * Generate checksum by parameters we have in body
+     * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys
+     */
+    PaytmChecksum.generateSignature(
+      JSON.stringify(paytmParams.body),
+      process.env.TESTMKEY
+    ).then(function (checksum) {
+      paytmParams.head = {
+        signature: checksum,
+      };
+
+      var post_data = JSON.stringify(paytmParams);
+
+      var options = {
+        /* for Staging */
+         hostname: 'securegw-stage.paytm.in' /* for Production */, 
+        //hostname: 'securegw.paytm.in',
+        port: 443,
+        path: `/theia/api/v1/initiateTransaction?mid=${process.env.TESTMID}&orderId=${data.orderId}`,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': post_data.length,
+        },
+      };
+
+      var response = '';
+      var post_req = https.request(options, function (post_res) {
+        post_res.on('data', function (chunk) {
+          response += chunk;
+        });
+
+        post_res.on('end', function () {
+          console.log('Response: ', response);
+          const value = JSON.parse(response);
+          res.json({
+            success: true,
+            data,
+            value,
+          });
+        });
+      });
+
+      post_req.write(post_data);
+      post_req.end();
+    });
+  } catch (error) {
+    res.send(error);
+  }
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
